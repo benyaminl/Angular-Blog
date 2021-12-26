562177027b0acbb92c91b77c03fb8dab0e122e87
Benyamin Limanto <blbenyamin9@gmail.com>
2021-12-26 21:15:22 +0700

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-blog';
+  logginHidden = 'none';
+
+  public loginComponent = false;
+
+  btnShowLogin() {
+    this.logginHidden = (this.logginHidden == 'none') ? 'block' : 'none';
+  }
 }
